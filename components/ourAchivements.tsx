0748213b9e28a71d1faa4c24ceb739736381ab11
mani sudheer <manisudheer383@gmail.com>
2025-10-01 19:39:57 +0530

    changes

--- components/ourAchivements.tsx
+++ components/ourAchivements.tsx
@@ -116,7 +116,7 @@ export default function AchievementsSection() {
         </div>
         <div className="bg-gray-900 rounded-2xl overflow-hidden border-2 border-primary">
           <img
-            src="/images/karate17.jpg"
+            src="/images/2ndWorld.jpg"
             alt="2nd World Martial Arts Championship Gold Medalist"
             className="w-full h-40 object-cover"
           />
